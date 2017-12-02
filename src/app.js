049a27889bb189663556a04ab9ac2cc23fe4b8e4
Nevan Tan <twoleet4u@gmail.com>
2017-12-02 13:46:20 -0500

    Setup webpack with babel and add React and ReactDOM node modules

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-//import './utils.js';
-//import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 80));
-
-import isSenior, {isAdult, canDrink} from './person.js';
-console.log(isAdult(18));
-console.log(canDrink(19));
-console.log(isSenior(64));
+const template = <p>testing 123</p>;
+ReactDOM.render(template, document.getElementById('app'));
